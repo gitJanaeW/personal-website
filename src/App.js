00a6a80d687ae9ea8392a7422b2b9ccd3b09dfdc
janae.wel <janae.wel@gmail.com>
2022-11-18 22:01:35 -0500

    finished footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 const App = () => {
     return(
@@ -7,6 +8,7 @@ const App = () => {
             <Nav/>
             <h1>Hello, React, Webpack and Bootstrap</h1>
             <button className="btn bg-primary bg-gradient">Primary button</button>
+            <Footer/>
         </div>
     );
 }
